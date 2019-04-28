4489199a36c54f772b4125335af56d4a4ea9e875
sadaki244 <sadaki.m244@gmail.com>
2019-04-28 15:36:42 +0900

    GoForwardUnityChanの課題です。ご確認宜しくお願い致します。

---
+++ Assets/Assets/BackgroundController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+
+    private float scrollSpeed = -0.03f;
+    private float deadLine = -16;
+    private float startLine = 15.8f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        transform.Translate(this.scrollSpeed, 0, 0);
+
+        if (transform.position.x < this.deadLine)
+        {
+            transform.position = new Vector2(this.startLine, 0);
+        }
+    }
+}
---
+++ Assets/Assets/CubeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    private float speed = -0.2f;
+
+    private float deadLine = -10;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        transform.Translate(this.speed, 0, 0);
+
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "CubeTag")
+        {
+            audioSource.Play();
+        }
+
+        if (collision.gameObject.tag == "GroundTag")
+        {
+            audioSource.Play();
+        }
+
+    }
+
+}
